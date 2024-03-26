45ec49345ff43e3396c8f698cd95151e5a69c9fd
Farzan Saeidi <farzan.saeidi.1@ulaval.ca>
2024-03-26 17:00:43 -0400

    Remise Farzan Saeidi

---
+++ db_scripts/rollback_1.sql
@@ -0,0 +1,5 @@
+ALTER TABLE musician DROP COLUMN role;
+ALTER TABLE musician DROP COLUMN bandName;
+ALTER TABLE musician CHANGE COLUMN musicianName singerName VARCHAR(50);
+ALTER TABLE musician RENAME TO singer;
+DROP TABLE band;
